afc4ba4e73819920b790479cc62452008ecbf13a
Aspix84 <aspixdas@gmail.com>
2025-03-26 17:52:13 +0200

    clean file app.js

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-// const array = [1, 2, 3, 4, 5];
-// console.log(array);
-
 const titleElement = document.querySelector('#title')
 const createBtn = document.querySelector('#create')
 const listElement = document.querySelector('#list')
